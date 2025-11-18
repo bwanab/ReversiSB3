975a937b916fed3e3785670c02cbee983e4e52cc
Bill Allen <bill@k2bea.org>
2025-11-18 10:22:42 -0500

    Update the code for model opponent to work correctly 2

--- sb-play.py
+++ sb-play.py
@@ -1,6 +1,6 @@
 
 import gymnasium as gym
-from util.reversi import ReversiEnvCNN
+from util.reversi import build_reversi
 from util.play import play
 from util.opponents import get_opponent
 
@@ -10,13 +10,13 @@ torch.device("cpu") # torch.device("mps")
 from sb3_contrib import MaskablePPO
 from sb3_contrib.common.maskable.policies import MaskableActorCriticPolicy
 
-def play_games(file, num_games=100, verbose=False, opponentName="Random", deterministic=False):
-    env = ReversiEnvCNN.build_reversi(opponent=opponentName, verbose=verbose)
-    opponent = get_opponent(opponentName, file=file, env=env)
+def play_games(file, num_games=100, verbose=False, opponentName="Random", deterministic=False, opponent_model = None):
+    env = build_reversi(opponent=opponentName, verbose=verbose, opponent_model=opponent_model)
+    #opponent = get_opponent(opponentName, file=file, env=env)
 
     model = MaskablePPO.load(file, env=env)
     # model.policy = MaskableActorCriticPolicy.load(file + '_policy.zip')
-    black_wins = play(model, num_games, opponent, deterministic, verbose)
+    black_wins = play(model, num_games, None, deterministic, verbose)
     return black_wins
 
 import argparse
@@ -26,9 +26,10 @@ if __name__ == '__main__':
                     description = 'meant to train a reversi ml, current just doing tests',
                     epilog = 'Text at the bottom of help')
 
-    parser.add_argument("-e", "--episodes", default=100)
-    parser.add_argument("-m", "--model", default = "dork8_CNN_test")
-    parser.add_argument("-o", "--opponent", default="Random")
+    parser.add_argument("-e", "--episodes", default=10)
+    parser.add_argument("-m", "--model", default = "dorkF_CNN_test")
+    parser.add_argument("-r", "--opp_model", default = "dorkE_CNN_test")
+    parser.add_argument("-o", "--opponent", default="Model")
     parser.add_argument("-d", "--non_deterministic", action='store_true')
     parser.add_argument("-v", "--verbose", action='store_true')
     args = parser.parse_args()
@@ -38,7 +39,12 @@ if __name__ == '__main__':
     # black_wins = play_games("models/" + args.model, n_games, verbose=True, opponentName=args.opponent,deterministic=args.deterministic)
     start = time.time()
     deterministic = not bool(args.non_deterministic)
-    black_wins = play_games("models/" + args.model, n_games, verbose=bool(args.verbose), opponentName=args.opponent,deterministic=deterministic)
+    black_wins = play_games("models/" + args.model,
+                            n_games, 
+                            verbose=bool(args.verbose), 
+                            opponentName=args.opponent,
+                            deterministic=deterministic, 
+                            opponent_model=args.opp_model)
     end = time.time()
     print(f"time elapsed: {end - start}")
     print(f"Black wins: {100 * black_wins / n_games}%")
